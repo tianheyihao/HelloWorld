4201d067d1a7e0bde4963e7a9abadf2ad4165974
qwer <1776160822@qq.com>
2024-09-08 10:20:36 +0800

    创建了main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void sort(int arr[],int size)
+{
+
+}
+
+int main(void) {
+    int  arr[] = {1,2,3,4,5,6};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    sort(arr,size);
+    return 0;
+}
